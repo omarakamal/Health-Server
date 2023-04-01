5b7dbea752133da35e42456666de592d2d9d3ac6
Omar <omaraakamal@gmail.com>
2023-04-02 03:44:01 +0400

    server

---
+++ models/Appointment.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const appointmentSchema = new Schema(
+  {
+    // doctorId: {type: Schema.Types.ObjectId, ref: "Doctor"},
+    // patientId: {type: Schema.Types.ObjectId, ref: "Patient"},
+    doctorId: { type: String, ref: "Doctor" },
+    patientId: { type: String, ref: "Patient"},
+    dateTime: 
+    {
+      type: String,
+      required: [true, "Please enter a date and time"],
+    },
+    department: {
+      type: String,
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Appointment = model("Appointment", appointmentSchema);
+
+module.exports = Appointment;
---
+++ models/Patient.model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const patientSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "User name is required,"],
+            unique: true,
+        },
+        email: {
+          type: String,
+          required: [true, "Email is required."],
+          lowercase: true,
+          trim: true,
+        },
+        photo: {
+            type: String,
+            trim: true,
+        },
+        dob: {
+             type: String
+        },
+        gender: {
+            type: [String],
+            enum: ["M", "F", "N/A"],
+        },
+        bloodType: {
+          type: [String],
+          enum: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"]
+        }
+      },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Patient = model("Patient", patientSchema);
+
+module.exports = Patient;
